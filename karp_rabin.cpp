4fa2e5984e8232d0293814b0a111df9cf39d59ca
shulgaalexey <shulgaalexey@gmail.com>
2015-04-01 23:58:30 +0900

    Updated comment for Karp-Rabin alg

--- karp_rabin.cpp
+++ karp_rabin.cpp
@@ -1,8 +1,6 @@
-// 4.8 You have a very large binary trees: T1, with millions of nodes, and T2, with hundreds of nodes.
-// Create an algorithm to decide if T2 is a subtree of T1.
-// A tree T2 is a subtree of  T1 if there exists a node n in T1 such that the subtree of
-// n is identical to T2. That is, if you cut off the tree at node n, the two trees would
-// be identical.
+// Algorithm by Karp-Rabin: finding a substring in another string
+//
+//
 #include <iostream>
 #include <math.h> // for trace
 #include <algorithm>
